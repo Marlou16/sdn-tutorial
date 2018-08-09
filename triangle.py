8d763d93d42036e7f317d968bc9f8126c4c2041b
Marlou Pors <marloupors@gmail.com>
2018-08-09 21:11:35 +0200

    Fix indentation in triangle.py

--- triangle.py
+++ triangle.py
@@ -17,17 +17,17 @@ class MyTopo( Topo ):
         # Add hosts and switches
         host1 = self.addHost( 'h1' )
         host2 = self.addHost( 'h2' )
-		host3 = self.addHost( 'h3' )
+	host3 = self.addHost( 'h3' )
         leftSwitch = self.addSwitch( 's1' )
         rightSwitch = self.addSwitch( 's2' )
-		upperSwitch = self.addSwitch( 's3' )
+	upperSwitch = self.addSwitch( 's3' )
 
         # Add links
         self.addLink( host1, leftSwitch )
-		self.addLink( host2, rightSwitch )
-		self.addLink( host3, upperSwitch )
+	self.addLink( host2, rightSwitch )
+	self.addLink( host3, upperSwitch )
         self.addLink( leftSwitch, rightSwitch )
-		self.addLink( leftSwitch, upperSwitch )
+	self.addLink( leftSwitch, upperSwitch )
         self.addLink( upperSwitch, rightSwitch )
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
